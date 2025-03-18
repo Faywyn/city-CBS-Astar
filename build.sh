3bdf55feb80085c14c0ac79508d8da580b6abf15
Alban <alban.dllr@gmail.com>
2025-03-18 11:41:06 +0100

    Update doc building

--- build.sh
+++ build.sh
@@ -18,8 +18,19 @@ elif [ "$type" == "releaseRun" ]; then
   ./build/bin/city-cbs-astar $args
 elif [ "$type" == "clean" ]; then
   rm -rf build
+  rm -rf doc/latex
 elif [ "$type" == "run" ]; then
   ./build/bin/city-cbs-astar $args
+elif [ "$type" == "doc" ]; then
+  echo "Warning: Create the latex files using doxygen first"
+  if [ -d "doc/latex" ]; then
+    cd doc/latex
+    make
+    cd ../.. 
+    cp doc/latex/refman.pdf doc/documentation.pdf
+  else
+    echo "Error: The latex files are not created"
+  fi
 elif [ "$type" == "sign" ]; then
   codesign --entitlements Entitlements.plist -s - ./build/bin/city-cbs-astar
 elif [ "$type" == "help" ]; then
@@ -27,6 +38,7 @@ elif [ "$type" == "help" ]; then
   echo "Usage: ./build.sh [debugRun|releaseRun] [num_agents_min] [num_agents_max] [num_data]: Build and run the project in debug or release mode"
   echo "Usage: ./build.sh [clean]: Clean the build directory"
   echo "Usage: ./build.sh [run] [num_agents_min] [num_agents_max] [num_data]"
+  echo "Usage: ./build.sh [doc]: Create the documentation (doxygen and latex)"
 else
-  echo "Usage: ./build.sh [debug|debugRun|release|releaseRun|clean|run|help]"
+  echo "Usage: ./build.sh [debug|debugRun|release|releaseRun|clean|doc|run|help]"
 fi
